abf7582672d1b4742913a86d722d8284a43e6e32
uNik020 <shekharnikhil020@gmail.com>
2025-11-13 13:31:29 +0530

    first commit

---
+++ reportupdates/reports/report/top_100_most_selling_item_which_are_not_in_stock/top_100_most_selling_item_which_are_not_in_stock.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, sai and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Top 100 Most selling Item which are not in stock"] = {
+	"filters": [
+
+	]
+};
---
+++ reportupdates/reports/report/top_most_selling_item_which_are_out_of_stock/top_most_selling_item_which_are_out_of_stock.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, sai and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Top Most selling Item which are out of stock"] = {
+	"filters": [
+
+	]
+};
